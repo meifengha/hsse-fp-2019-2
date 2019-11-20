f74e8b61be210d079ee23b1e69bfbc6c8030f80b
Artur_Nurtdinov <a.nurtdinow@yandex.ru>
2019-11-21 01:19:13 +0300

    feat: Implement of hamming distance function

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,7 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys
+  | null xs || null ys = Just 0
+  | length xs /= length ys = Nothing
+  | otherwise = Just (length (filter id (zipWith (/=) xs ys)))
