1a3e91dddc1b054a4ba879507be53ce95a4ac115
Georgii <corradowave1@gmail.com>
2019-11-21 22:10:27 +0300

    init

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,28 +1,32 @@
-package recfun
-import common._
+object Main extends App {
 
-object Main {
-  def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
+  println("Pascal's Triangle")
+  for (row <- 0 to 10) {
+    for (col <- 0 to row)
+      print(pascal(col, row) + " ")
+    println()
   }
+
+  println("Balancing")
+  val chars: List[Char] = List('a', 'b', '(', ')')
+  println(balance(chars))
+
+  println("Counting change")
+  println(countChange(5, List(5)))
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    chars.count(_ == '(') == chars.count(_ == ')')
   }
 
   /**
@@ -32,7 +36,11 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
 
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty) 0
+    else if (money - coins.head == 0) 1
+    else if (money - coins.head < 0) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
   }
 }
