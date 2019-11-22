6e8e0271a39d40fd6bea4c21f3d110cf89fc5f00
Vlad Zybkin <zybkin11@gmail.com>
2019-11-22 17:50:48 +0300

    added impl of haskell lab work

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,9 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys =  if null xs || null ys then Just 0
+                                  else if (length xs /= length ys) then Nothing
+                                  else if (length xs == 0) then Just 0
+                                  else case distance (tail xs) (tail ys) of
+                                       Just n -> if head xs /= head ys then Just (1 + n) else Just n
+                                       Nothing -> Nothing
