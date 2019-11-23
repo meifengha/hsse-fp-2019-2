2446245bedf722f4b187235cc3c5df09264c3480
Maria Potapova <potapova.ma@mail.ru>
2019-11-23 19:55:29 +0300

    Initial commit with scala - 1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,28 +1,73 @@
 package recfun
-import common._
 
-object Main {
-  def main(args: Array[String]) {
+object Main
+{
+  def main(args: Array[String])
+  {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
+    for (y <- 0 to 10) {
+      for (x <- 0 to y)
+        print(pascal(x, y) + " ")
       println()
     }
+
+    println(balance("()(())()".toCharArray.toList))
+    println(balance("()(()()".toCharArray.toList))
+
+    println(countChange(7, List(1, 2, 3)))
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
 
+  def pascal(x: Int, y: Int): Int =
+  {
+    if (x == 0 || x == y)
+    {
+      1
+    }
+    else
+    {
+      pascal(x - 1, y -1) + pascal(x, y - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
+
+  def balance(chars: List[Char]): Boolean =
+  {
+    def balanceIt(position: Int, count: Int): Boolean =
+    {
+
+      if (position == chars.length)
+      {
+        count == 0
+      }
+      else if (chars(position) == '(')
+      {
+        balanceIt(position + 1, count + 1)
+      }
+      else if (chars(position) == ')')
+      {
+        if (count == 0)
+        {
+          false
+        }
+        else
+        {
+          balanceIt(position + 1, count - 1)
+        }
+       }
+       else
+       {
+         balanceIt(position + 1, count)
+       }
+    }
+
+    balanceIt(0, 0)
   }
 
   /**
@@ -32,7 +77,25 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
 
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.contains(0))
+    {
+      0;
+    }
+    if (money == 0)
+    {
+      1;
+    }
+    else if (money < 0 || coins.isEmpty)
+    {
+      0;
+    }
+    else
+    {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
   }
 }
+
+© 2019 GitHub, Inc.
