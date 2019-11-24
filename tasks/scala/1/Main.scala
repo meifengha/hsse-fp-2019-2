7dbf7f3c123d9cd149ef28aac30d40f4f2e92362
Viktoriia <sweetydarkyrosy@gmail.com>
2019-11-24 18:34:37 +0300

    First and second Scala tasks completed

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,37 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 | c == r)
+    {
+      1
+    } else {
+      pascal(c-1, r-1) + pascal(c, r-1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def subFunc(chars: List[Char], opens: Int): Boolean = {
+      if (chars.isEmpty) {
+        opens == 0
+      }
+      else if (chars.head == '(') {
+        subFunc(chars.tail, opens + 1)
+      }
+      else if (chars.head == ')' && opens > 1) {
+        subFunc(chars.tail, opens - 1)
+      }
+      else if (opens >= 0) {
+        subFunc(chars.tail, opens)
+      }
+      else {
+        false
+      }
+    }
+
+    subFunc(chars, 0)
   }
 
   /**
@@ -33,6 +56,23 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
+    var amount = 0;
+    def subFunc(money: Int, coins: List[Int]): Any = {
+      if (!coins.isEmpty) {
+        if (money > coins.head) {
+          subFunc(money - coins.head, coins)
+          subFunc(money, coins.tail)
+        }
+        else if (money < coins.head) {
+          subFunc(money, coins.tail)
+        }
+        else if (money-coins.head == 0) {
+          amount += 1
+        }
+      }
+    }
 
+    subFunc(money, coins.sorted)
+    amount
   }
 }
