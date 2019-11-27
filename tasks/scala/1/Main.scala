e984f138098d4fa1a66492b75a21d1d648fd3295
Tolstikov Grigoriy <tolstikov.gn@edu.spbstu.ru>
2019-11-27 22:53:42 +0300

    1.2. Main and `balance` implementation.

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -4,13 +4,50 @@ package recfun
 
 object Main {
   def main(args: Array[String]) {
+    println("\n*****EXERCISE1*****\n")
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
     }
+
+
+    println("\n*****EXERCISE2*****\n")
+    val trueString1 = "sa1233(wq)(((aaczarq1)))cz"
+    val trueString2 = "12431"
+    val trueString3 = "(12341yadqz)"
+    val falseString1 = ")(("
+    val falseString2 = "11(1.c1z2))z"
+
+    println("True:")
+    println(balance(trueString1.toList), balance(trueString2.toList), balance(trueString3.toList))
+    println("False:")
+    println(balance(falseString1.toList), balance(falseString2.toList))
+
+    println("\n*****EXERCISE3*****\n")
+    val coins1: List[Int] = List(1, 2, 5, 10)
+    val coins2: List[Int] = List(2, 3)
+
+    println("coins: " + coins1)
+    var money = 5
+    println("money: " + money + "\nways to change: " + countChange(money, coins1))
+    money = 0
+    println("money: " + money + "\nways to change: " + countChange(money, coins1))
+    money = 123
+    println("money: " + money + "\nways to change: " + countChange(money, coins1) + "\n")
+
+    println("coins: " + coins2)
+    money = 5
+    println("money: " + money + "\nways to change: " + countChange(money, coins2))
+    money = 1
+    println("money: " + money + "\nways to change: " + countChange(money, coins2))
+    money = 11
+    println("money: " + money + "\nways to change: " + countChange(money, coins2))
+    money = 34
+    println("money: " + money + "\nways to change: " + countChange(money, coins2) + "\n")
   }
+
 
   /**
    * Exercise 1
@@ -21,20 +58,24 @@ object Main {
   /**
    * Exercise 2 Parentheses Balancing
    */
+  var count = 0
   def balance(chars: List[Char]): Boolean = {
-
+    if (chars.isEmpty) return (count == 0)
+    else if (chars.head == '(') count += 1
+    else if (chars.head == ')') count -= 1
+    if (count < 0) {
+      count = 0;
+      return false
+    }
+    balance(chars.tail)
   }
 
   /**
    * Exercise 3 Counting Change
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    if (money == 0) {
-      1
-    } else if (coins.isEmpty || (money < 0)) {
-      0
-    } else {
-      countChange(money, coins.tail) + countChange(money - coins.head, coins)
-    }
+    if (money == 0) 1
+    else if (coins.isEmpty || (money < 0)) 0
+    else countChange(money, coins.tail) + countChange(money - coins.head, coins)
   }
 }
