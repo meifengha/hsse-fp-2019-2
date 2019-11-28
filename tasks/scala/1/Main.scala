4d9f2d025ec9f349e4948ae759e10a2883f36967
TrueCoder <aleksandrv1999@gmail.com>
2019-11-28 13:26:43 +0300

    First commit lab1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,15 +15,29 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
-  }
+      if (c == 0 || r == 0 || c == r) 1
+      else if (c > r) 0
+      else pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    balance(chars, 0)
   }
+  
+  def balance(chars: List[Char], unbalancedCount: Int): Boolean = {
+      if (chars.isEmpty) unbalancedCount == 0
+      else if (unbalancedCount < 0) false
+      else {
+        val count =
+          if (chars.head == '(') unbalancedCount + 1
+          else if (chars.head == ')') unbalancedCount - 1
+          else unbalancedCount
+        balance(chars.tail, count)
+      }
+    }
 
   /**
    * Exercise 3 Counting Change
@@ -33,6 +47,13 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+     if (money <= 0) 0
+     else countChangeAux(money, coins.sorted.reverse)
   }
+  
+  def countChangeAux(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty || money < 0) 0
+    else if (money == 0) 1
+    else countChangeAux(money - coins.head, coins) + countChangeAux(money, coins.tail)
+   }
 }
