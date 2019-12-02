9f9c513f2ee5d3cae4fb0e843b4016c71e1dcaaa
Katerina <zaitceva_ek@mail.ru>
2019-12-02 15:04:14 +0300

    scala 1 init

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,22 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (r == 0) || (c == r)) { 1 }
+    else { pascal(c - 1, r - 1) + pascal(c, r - 1) }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    var countOpenParenthesis: Int = 0
+    var countCloseParenthesis: Int = 0
+    for (i <- 0 until chars.length) {
+      if (chars(i) == '(') { countOpenParenthesis += 1 }
+      if (chars(i) == ')') { countCloseParenthesis += 1 }
+    }
+    if (countOpenParenthesis == countCloseParenthesis) { true }
+    else { false }
   }
 
   /**
@@ -33,6 +41,8 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if ((coins.isEmpty) || (money < 0)) { 0 }
+    else if (money == 0) { 1 }
+    else { countChange(money, coins.tail) + countChange(money - coins.head, coins) }
   }
 }
