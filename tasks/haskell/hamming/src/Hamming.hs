ba1648df3202b58e50a371ca01df51c2414658a9
Полина Московская <polinamosk530@gmail.com>
2019-12-02 19:04:48 +0300

    Haskell initial

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,11 @@
 module Hamming (distance) where
+import Data.Char (isSpace)
+trim :: String -> String
+trim = f . f
+  where f = reverse . dropWhile isSpace
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys =
+ if null xs || null ys then Just 0
+ else if length (trim xs) /= length (trim ys) then Nothing
+ else Just (length (filter id (zipWith (/=) (trim xs) (trim ys))))
