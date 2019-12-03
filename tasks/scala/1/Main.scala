d39001d71f8d21bbf4e1131a690e04d378088c4a
elenastotskaya <elstotskaya@gmail.com>
2019-12-03 12:47:30 +0300

    Initial commit

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -9,30 +8,47 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println(balance(List('(', '(', 'c', 'o', 'r', 'r', 'e', 'c', 't', ')', 'l', 'i', 's', 't', ')')))
+    println(balance(List('(', ')', 'w', 'r', 'o', 'n', 'g', '(')
+    println(countChange(6, List(1, 2)))
   }
 
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-
-  }
-
+    def pascal(c: Int, r: Int): Int = {
+      def rowCount(x: Int, k: Int): Int =
+        if (k == c + 1) x else rowCount(x*(r + 1 - k)/k, k - 1)
+      rowCount(1, 1)
+    }
+  
   /**
-   * Exercise 2 Parentheses Balancing
+   * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = {
-   
-  }
-
+    def balance(chars: List[Char]): Boolean = {
+      def countBrackets(chars: List[Char], count: Int): Boolean = {
+        if (chars.isEmpty) count == 0 else {
+          if (chars.head == '(')
+            countBrackets(chars.tail, count + 1)
+          else if (chars.head == ')')
+            (count >= 0) && (countBrackets(chars.tail, count - 1))
+          else
+            countBrackets(chars.tail, count)
+         }
+      }
+      countBrackets(chars, 0)
+    }
+  
   /**
-   * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
+   * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+    def countChange(money: Int, coins: List[Int]): Int = {
+      if (money == 0) {
+        1
+      } else if ((money < 0) || coins.isEmpty) {
+        0
+      } else {
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      }
+    }
   }
-}
