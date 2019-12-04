56191cee8d424bc1dbe8cbe582a1c9078f91caf3
Trang68 <55749300+Trang68@users.noreply.github.com>
2019-12-04 13:45:50 +0300

    haskel lab

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,8 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance [] [] = Just 0
+distance _ _ = Nothing
+distance (x:xs) (y:ys)
+      		 | x /= y = fmap (1 +)(distance xs ys)
+     		 | otherwise = distance xs ys
