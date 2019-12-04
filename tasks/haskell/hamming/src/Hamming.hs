ca8eafa5fdea4ca792769c85c96ba08c80322bfc
Trang68 <55749300+Trang68@users.noreply.github.com>
2019-12-04 13:46:39 +0300

    haskell

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -4,5 +4,5 @@ distance :: String -> String -> Maybe Int
 distance [] [] = Just 0
 distance _ _ = Nothing
 distance (x:xs) (y:ys)
-      		 | x /= y = fmap (1 +)(distance xs ys)
-     		 | otherwise = distance xs ys
+        | x /= y = fmap (1 +)(distance xs ys)
+     	  | otherwise = distance xs ys
