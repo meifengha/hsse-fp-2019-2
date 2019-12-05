102defe0d49ee68ec94484c8e4d009b652ed8da3
am-spbstu <altuhov.ma@edu.spbstu.ru>
2019-12-05 13:10:21 +0300

    Haskell Task: initial commit

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -2,3 +2,6 @@ module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
 distance xs ys = error "Implementation is lost..."
+  | null xs || null ys = Just 0
+  | length xs /= length ys = Nothing
+  | otherwise = Just (sum [1 | pair <- zip xs ys, fst pair /= snd pair])
