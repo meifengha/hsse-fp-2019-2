bc0b351a09d74c9995cf804037cbe93fdfcc0641
Kraynov Alexander <kraynovsasha@gmail.com>
2019-12-07 22:51:35 +0300

    haskel init

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,7 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = sum differences
+  where
+    differences = zipWith areDifferent xs ys
+    areDifferent a b = if a /= b then 1 else 0
