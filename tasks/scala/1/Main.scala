b972f9cdf8210a5eec3bef310f17f57730d1768f
Daniil Klimchuk <dan.klimchuk@yandex.ru>
2019-12-07 20:29:28 +0300

    scala and haskell labs

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,12 +1,17 @@
 package recfun
 import common._
 
-object Main {
-  def main(args: Array[String]) {
+object Main
+{
+  def main(args: Array[String])
+  {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
+    for (row <- 0 to 10)
+    {
       for (col <- 0 to row)
-        print(pascal(col, row) + " ")
+        {
+          print(pascal(col, row) + " ")
+        }
       println()
     }
   }
@@ -14,15 +19,43 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = {
-
+  def pascal(c: Int, r: Int): Int =
+  {
+    if ((c == r) || (c == 0))
+      {
+        1
+      }
+    else
+      {
+        pascal(c - 1, r - 1) + pascal(c, r - 1)
+      }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
-  def balance(chars: List[Char]): Boolean = {
-   
+  def balance(chars: List[Char]): Boolean =
+  {
+    def checkBalance(chars: List[Char], count: Int): Boolean =
+      {
+        if (chars.isEmpty)
+          {
+            count == 0
+          }
+        else if (chars.head == '(')
+          {
+            checkBalance(chars.tail, count + 1)
+          }
+        else if (chars.head == ')')
+          {
+            (checkBalance(chars.tail, count - 1)) && (count > 0)
+          }
+        else
+          {
+            checkBalance(chars.tail, count)
+          }
+      }
+    checkBalance(chars, 0)
   }
 
   /**
@@ -32,7 +65,19 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
-  def countChange(money: Int, coins: List[Int]): Int = {
-
+  def countChange(money: Int, coins: List[Int]): Int =
+  {
+    if (money == 0)
+      {
+        1
+      }
+    else if ((money < 0) || (coins.isEmpty))
+      {
+        0
+      }
+    else
+      {
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      }
   }
 }
