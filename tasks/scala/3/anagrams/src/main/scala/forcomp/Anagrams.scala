461d8a3405124acf1b883e25af6f66ea2a886903
Maria Potapova <potapova.ma@mail.ru>
2019-12-08 02:34:25 +0300

    scala task 3

--- tasks/scala/3/anagrams/src/main/scala/forcomp/Anagrams.scala
+++ tasks/scala/3/anagrams/src/main/scala/forcomp/Anagrams.scala
@@ -33,10 +33,10 @@ object Anagrams {
    *  Note: the uppercase and lowercase version of the character are treated as the
    *  same character, and are represented as a lowercase character in the occurrence list.
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences = (for ((ch, list) <- (w.toLowerCase groupBy (ch => ch))) yield ch -> list.size).toList.sorted
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.foldLeft("")(_ + _))
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
@@ -53,10 +53,10 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary groupBy (wordOccurrences(_)) withDefaultValue List()
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
@@ -80,7 +80,16 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = occurrences match {
+      case List() => List(List())
+      case head :: tail => {
+        val tailCombinations = combinations(tail)
+        tailCombinations ++ (for {
+          combination <- tailCombinations
+          i <- 1 to head._2
+        } yield (head._1, i) :: combination)
+      }
+    }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    * 
@@ -92,8 +101,12 @@ object Anagrams {
    *  Note: the resulting value is an occurrence - meaning it is sorted
    *  and has no zero-entries.
    */
-  def subtract(x: Occurrences, y: Occurrences): Occurrences = ???
-
+  def subtract(x: Occurrences, y: Occurrences): Occurrences =
+      y.foldLeft(x.toMap)((acc, b) => {
+          val occ = acc(b._1) - b._2
+          if (occ == 0) acc - b._1
+          else acc.updated(b._1, occ)
+      }).toList.sorted
   /** Returns a list of all anagram sentences of the given sentence.
    *  
    *  An anagram of a sentence is formed by taking the occurrences of all the characters of
@@ -134,6 +147,43 @@ object Anagrams {
    *
    *  Note: There is only one anagram of an empty sentence.
    */
-  def sentenceAnagrams(sentence: Sentence): List[Sentence] = ???
+  def sentenceAnagrams(sentence: Sentence): List[Sentence] = {
+      def sentenceAnagrams(occurrences: Occurrences): List[Sentence] = {
+        if (occurrences.isEmpty) List(List())
+        else for {
+          combination <- combinations(occurrences)
+          word <- dictionaryByOccurrences(combination)
+          result <- sentenceAnagrams(subtract(occurrences, combination))
+        } yield word :: result
+      }
+      sentenceAnagrams(sentenceOccurrences(sentence))
+    }
+  }
 
+  object Dictionary {
+    def loadDictionary: List[String] = {
+      val wordstream = Option
+      {
+        getClass.getResourceAsStream(List("forcomp", "linuxwords.txt").mkString("/", "/", ""))
+      } getOrElse
+      {
+        sys.error("Could not load word list, dictionary file not found")
+      }
+      try
+      {
+        val s = scala.io.Source.fromInputStream(wordstream)
+        s.getLines.toList
+      }
+      catch
+      {
+        case e: Exception =>
+          println("Can`t load word list: " + e)
+          throw e
+      }
+      finally
+      {
+        wordstream.close()
+      }
+    }
+  }
 }
