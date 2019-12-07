8174bed696fd4890029cea15fee9be9d4375d4a0
am-spbstu <altuhov.ma@edu.spbstu.ru>
2019-12-08 02:12:35 +0300

    Scala #2: minor syntax and style changes

--- tasks/scala/2/src/main/scala/funsets/FunSets.scala
+++ tasks/scala/2/src/main/scala/funsets/FunSets.scala
@@ -26,24 +26,24 @@ object FunSets {
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = x => s(x) || t(x)
+  def union(s: Set, t: Set): Set = x => contains(s, x) || contains(t, x)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = x => s(x) && t(x)
+  def intersect(s: Set, t: Set): Set = x => contains(s, x) && contains(t, x)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = x => intersect(s, !t(_))
+  def diff(s: Set, t: Set): Set = intersect(x, !t(_))
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = x => intersect(s, p)
+  def filter(s: Set, p: Int => Boolean): Set = intersect(s, p)
   
 
   /**
