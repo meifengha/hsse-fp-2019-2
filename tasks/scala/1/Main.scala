ee28885f82fc5c077923f593dab52c91ff322d3b
SokoloVa-21 <55749776+SokoloVa-21@users.noreply.github.com>
2019-12-10 16:09:15 +0300

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,14 +15,32 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (r == 0) || (c == r)) { 
+      1 
+    } else { 
+      pascal(c - 1, r - 1) + pascal(c, r - 1) 
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    var cOpenParenth: Int = 0
+    var cCloseParenth: Int = 0
+    for (i <- 0 until chars.length) {
+      if (chars(i) == '(') { 
+        cOpenParenth += 1 
+      }
+      if (chars(i) == ')') { 
+        cCloseParenth += 1 
+      }
+    }
+    if (cOpenParenth == cCloseParenth) {
+      true 
+    } else { 
+      false
+    }
   }
 
   /**
@@ -33,6 +51,12 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) {
+      1
+    } else if ((money < 0) || coins.isEmpty) {
+      0
+    } else {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
   }
 }
