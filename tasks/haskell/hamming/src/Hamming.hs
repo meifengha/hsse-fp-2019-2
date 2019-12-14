09ffe8458a1fe7776ff3935cbadd6a20d93994d4
Alexandra <adlovlac@gmail.com>
2019-12-14 22:28:36 +0300

    Hamming

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,12 @@
 module Hamming (distance) where
+import Data.List
+import Data.Maybe
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+
+distance xs ys | (length xs == length ys) = Just (implDistance xs ys 0)
+               | otherwise = Nothing
+
+implDistance xs ys acc | (length xs == 0) = acc
+               | (head xs /= head ys) = implDistance (tail xs) (tail ys) (acc + 1)
+               | otherwise = implDistance (tail xs) (tail ys) (acc)
