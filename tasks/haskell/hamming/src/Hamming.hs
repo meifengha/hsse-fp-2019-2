5afd4a5d6c1a058842cb60a00b45262f7e8a909b
yokowl <protasovmax@gmail.com>
2019-12-16 18:01:57 +0300

    Initial commit

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,6 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys | ((length xs) /= (length ys)) = Nothing | otherwise = Just (length(filter id (zipWith (/=) xs ys)))
+distance xs ys
+    | ((length xs) /= (length ys)) = Nothing
+    | otherwise = Just (length (filter id (zipWith (/=) xs ys)))
