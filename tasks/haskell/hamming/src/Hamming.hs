3a2f0c049754ce95743e13e2482d3b1ea59f318b
Малышев Антон <malyshev.tony@gmail.com>
2019-12-17 22:20:26 +0300

    Fix

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -2,9 +2,9 @@ module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
 
-distance xs ys =  if null xs || null ys then Just 0
-                  else if (length xs /= length ys) then Nothing
-                  else if (length xs == 0) then Just 0
-                  else case distance (tail xs) (tail ys) of
-                  Just n -> if head xs /= head ys then Just (1 + n) else Just n
-                  Nothing -> Nothing
+distance xs ys = if null xs || null ys then Just 0
+    else if (length xs /= length ys) then Nothing
+    else if (length xs == 0) then Just 0
+    else case distance (tail xs) (tail ys) of
+        Just n -> if head xs /= head ys then Just (1 + n) else Just n
+        Nothing -> Nothing
