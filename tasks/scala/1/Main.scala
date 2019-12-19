1ef660646f911641fcf3f06645482be72245756e
Elena Pedchenko <55746308+PedchenkoElena@users.noreply.github.com>
2019-12-19 19:56:36 +0500

    Update Main.scala
    Lab1

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -15,24 +15,52 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r)
+    {
+      1
+    }
+    else
+    {
+      pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def balancing(position: Int, openBraces: Int): Boolean = {
+      if (position == chars.length)
+        openBraces == 0
+      else if (chars(position) == '(')
+        balancing(position + 1, openBraces + 1)
+      else if  (chars(position) == ')')
+        if (openBraces == 0)
+          false
+        else
+          balancing(position + 1, openBraces - 1)
+      else
+        balancing(position + 1, openBraces)
+    }
+    
+    balancing(0, 0)
   }
 
   /**
    * Exercise 3 Counting Change
-   * Write a recursive function that counts how many different ways you can make
-   * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
-   * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0)
+    {
+      1;
+    }
+    else if ((money < 0) || coins.isEmpty)
+    {
+      0;
+    }
+    else
+    {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
   }
 }
