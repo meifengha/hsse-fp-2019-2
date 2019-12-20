665ce29dacd526a86e601d1f335c41ba844bc7a7
saltykov.ilya <isalti99@gmail.com>
2019-12-20 22:39:21 +0300

    initial commit

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,4 +1,15 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = error "Implementation is lost..."
+distance xs ys = 
+    if length xs /= length ys 
+        then 
+            error "Len(x) != Len(y). Please enter strings with equal lengths"
+    else 
+        Just (
+            length (
+                filter id (
+                    zipWith (/=) xs ys
+                )
+            )
+        )
