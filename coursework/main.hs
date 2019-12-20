813872730fb887df62d85904acfe18808b9909a8
alexandragoncharova <alexandra.andronova.99@mail.ru>
2019-12-20 21:32:36 +0300

    cursovik

---
+++ coursework/main.hs
@@ -0,0 +1,12 @@
+data Token = PlusToken | IntToken Int deriving (Show)
+
+buildIntToken [] = []
+buildIntToken value = [IntToken (read value::Int)]
+
+tokenize :: [Char] -> [Token]
+tokenize list = let (a, b) = foldr breakToken ([], []) list in (buildIntToken a) ++ b
+      where
+        breakToken '+' (current, output) = ([], PlusToken : ((buildIntToken current) ++ output))
+        breakToken num (current, output) = (num:current, output)
+
+main = print "Start:"
