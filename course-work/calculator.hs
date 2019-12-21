8ab446bffe283a1926327fdb2638bf9404d56f58
Aleksandra <adlovlac@gmail.com>
2019-12-21 11:50:09 +0300

    Calculator with priorities

--- course-work/calculator.hs
+++ course-work/calculator.hs
@@ -1,17 +1,49 @@
 module Calculator where
 import Data.List
 import Data.Char
-calculator :: String -> Float
-count :: Float -> String -> String -> Float
-helper :: Float -> String -> [String] -> Float
 
-count a b "+" = a + read b :: Float
-count a b "-" = a - read b :: Float
-count a b "*" = a * read b :: Float
-count a b "/" = a / read b :: Float
+calculator :: String -> Float
+calculator expr = processToken [] [] (words expr)
+
+processToken :: [Float] -> [String] -> [String] -> Float
+processToken numbers [] [] | (length numbers == 1) = head numbers
+                           | otherwise = error "Error while counting expression"
+processToken numbers operations [] = processToken (count n2 n1 op : restN) restOp []
+                                     where op = head operations
+                                           restOp = tail operations
+                                           n1 = head numbers
+                                           n2 = head (tail numbers)
+                                           restN = tail (tail numbers)
+
+processToken numbers operations tokens | (isOp t == True) =
+                                          if (lenOp == 0) then processToken numbers (t : operations) (tail tokens)
+                                             else if (prior op >= prior t) then processToken (count n2 n1 op : restN) (t : restOp) (tail tokens)
+                                                else processToken numbers (t : operations) (tail tokens)
+                                       | otherwise = processToken ((read t :: Float) : numbers) operations (tail tokens)
+                                       where t = head tokens
+                                             op = head operations
+                                             restOp = tail operations
+                                             n1 = head numbers
+                                             n2 = head (tail numbers)
+                                             restN = tail (tail numbers)
+                                             lenOp = length operations
+
+count :: Float -> Float -> String -> Float
+count a b "+" = a + b
+count a b "-" = a - b
+count a b "*" = a * b
+count a b "/" = a / b
 count a b operation = error "Operation not permitted"
 
-helper res op tokens | (length tokens == 1) = count res (head tokens) op
-                     | otherwise = helper (count res (head (tokens)) op) (head (tail tokens)) (tail (tail tokens));
+isOp :: String -> Bool
+isOp "+" = True
+isOp "-" = True
+isOp "*" = True
+isOp "/" = True
+isOp op = False
 
-calculator expr = helper 0 "+" (words expr)
+prior "*" = 2
+prior "/" = 2
+prior "+" = 1
+prior "-" = 1
+prior op = 0
