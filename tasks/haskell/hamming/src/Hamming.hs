ddc66cdfd3b0ba2232417c4120e13b29425c7142
IliaPopov <elias.tocaras@gmail.com>
2019-12-21 20:57:09 +0300

    Revert "Update Hamming.hs"
    This reverts commit 2cb5e447cad0d1f1641a23956d4446fa11f67720.

--- tasks/haskell/hamming/src/Hamming.hs
+++ tasks/haskell/hamming/src/Hamming.hs
@@ -1,7 +1,4 @@
 module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
-   distance xs ys =
-      if length xs == length ys
-      then Just (length (filter id (zipWith (/=) xs ys)))
-      else Nothing
+distance xs ys = error "Implementation is lost..."
