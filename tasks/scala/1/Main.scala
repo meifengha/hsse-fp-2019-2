ccc08cd981d6539491b53f3321064265306d7dc8
pranichnikovaan <123456anp@mail.ru>
2019-12-23 03:29:22 +0300

    labs

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -8,6 +8,15 @@ object Main {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
+
+    print("Number at c: 3 r: 4 -> ")
+    println(pascal(3, 5))
+
+    print("Checking for bracket balance: a()afgr!-rrgr043(rervv) -> ")
+    println(balance("a()afgr!-rrgr043(rervv)".toList))
+
+    print("Couting variants to give change: 5 money by [2, 2, 1, 3]coins - >")
+    println(countChange(5, List(2, 2, 1, 3)))
     }
   }
 
@@ -15,14 +24,14 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+    if (c == 0 || c == r) 1 else pascal(c, r - 1) + pascal(c - 1, r - 1)
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    chars.count(_ == '(') == chars.count(_ == ')')
   }
 
   /**
@@ -33,6 +42,9 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) 1
+      else if (money > 0 && !coins.isEmpty)
+        countChange(money - coins.head, coins) + countChange(money, coins.tail)
+      else 0
   }
 }
