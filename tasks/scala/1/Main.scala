901ed49b8414c347aa8e0f11100bdc06af316ba1
Alekseev-Al <37519734+Alekseev-Al@users.noreply.github.com>
2019-12-24 18:53:04 +0300

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -14,15 +14,42 @@ object Main {
   /**
    * Exercise 1
    */
+  
   def pascal(c: Int, r: Int): Int = {
-
+    if ((c == 0) || (r == 0) || (c == r)) { 
+      1 
+    } else { 
+      pascal(c - 1, r - 1) + pascal(c, r - 1) 
+    }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
-   */
-  def balance(chars: List[Char]): Boolean = {
-   
+   */  
+  
+    def balance(chars: List[Char]): Boolean = {
+    def countParenth(list: List[Char], count: Int): Int = {
+      if (count < 0) {
+        count
+      }
+      else if (list.isEmpty) {
+        count
+      }
+      else if (list.head == '(') {
+        countParenth(list.tail, count + 1)
+      }
+      else if (list.head == ')') {
+        countParenth(list.tail, count - 1)
+      }
+      else countParenth(list.tail, count)
+    }
+
+    if (countParenth(chars, 0) == 0) {
+      true
+    }
+    else {
+      false
+    }
   }
 
   /**
@@ -32,7 +59,14 @@ object Main {
    * there is 1 way to give change for 5 if you have coins with denomiation
    * 2 and 3: 2+3.
    */
+  
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0) {
+      1
+    } else if ((money < 0) || coins.isEmpty) {
+      0
+    } else {
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+    }
   }
 }
