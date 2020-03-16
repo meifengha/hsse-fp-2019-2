aa01696dc232dc2d5d6be3ee2c135dfd88db9ba4
meifengha <55582229+meifengha@users.noreply.github.com>
2020-03-16 15:08:06 +0800

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -9,20 +9,52 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+    println("Are parentheses balanced?")
+    var list: List[Char] = List('(', '(', ')', '(', ')', ')')
+    println(balance(list))
+    println("Coins change problem")
+    var listOfCoins: List[Int] = List(2, 3, 1)
+    var sum = 10
+    print("Need get " + sum + " using ")
+    listOfCoins.foreach(el => print(el + " "))
+    println()
+    println("Count of way is " + countChange(sum, listOfCoins))
   }
 
   /**
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-
+      if ((c == r) || (c == 0))
+      {
+        1
+      }
+      else
+      {
+        pascal(c, r - 1) + pascal(c - 1, r - 1)
+      }
   }
 
   /**
    * Exercise 2 Parentheses Balancing
    */
   def balance(chars: List[Char]): Boolean = {
-   
+    def countParentheses(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty)
+      {
+        count == 0
+      }
+      else if (chars.head == '(')
+      {
+        countParentheses(chars.tail, count + 1)
+      }
+      else if (chars.head == ')')
+      {
+        (count > 0) && (countParentheses(chars.tail, count - 1))
+      }
+      else countParentheses(chars.tail, count)
+    }
+    countParentheses(chars, 0)
   }
 
   /**
@@ -33,6 +65,17 @@ object Main {
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-
+    if (money == 0)
+    {
+      1
+    }
+    else if ((money < 0) || (coins.isEmpty))
+    {
+      0
+    }
+    else
+    {
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    }
   }
 }
