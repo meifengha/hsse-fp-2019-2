76435a772ecc411a2beefbb0f7021201be8712f6
meifengha <55582229+meifengha@users.noreply.github.com>
2020-03-16 15:18:54 +0800

    Update Main.scala

--- tasks/scala/1/Main.scala
+++ tasks/scala/1/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -25,14 +24,14 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-      if ((c == r) || (c == 0))
-      {
-        1
-      }
-      else
-      {
-        pascal(c, r - 1) + pascal(c - 1, r - 1)
-      }
+    if ((c == r) || (c == 0))
+    {
+      1
+    }
+    else
+    {
+      pascal(c, r - 1) + pascal(c - 1, r - 1)
+    }
   }
 
   /**
@@ -61,21 +60,21 @@ object Main {
    * Exercise 3 Counting Change
    * Write a recursive function that counts how many different ways you can make
    * change for an amount, given a list of coin denominations. For example,
-   * there is 1 way to give change for 5 if you have coins with denomiation
+   * there is 1 way to give change for 5 if you have coins with denomination
    * 2 and 3: 2+3.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    if (money == 0)
-    {
-      1
-    }
-    else if ((money < 0) || (coins.isEmpty))
-    {
-      0
-    }
-    else
-    {
-      countChange(money - coins.head, coins) + countChange(money, coins.tail)
-    }
+   if (money == 0)
+   {
+     1
+   }
+   else if ((money < 0) || (coins.isEmpty))
+   {
+     0
+   }
+   else
+   {
+     countChange(money - coins.head, coins) + countChange(money, coins.tail)
+   }
   }
 }
